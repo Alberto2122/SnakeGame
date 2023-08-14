515f84b2253fb8235f8c339e83816ba3897edfef
Alberto Silva <135075028+Alberto2122@users.noreply.github.com>
2023-08-14 11:20:30 -0300

    Jogo da cobrinha

---
+++ cobra.js
@@ -0,0 +1,223 @@
+// Puxando informações do html para usar. 
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+// Puxando informações do html para usar. 
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
+const audio = new Audio("../jogo da cobra/audio/assets_audio.mp3")
+
+const size = 30
+
+let initialPosition = { x: 270, y: 240 }
+
+
+let snake = [initialPosition]
+
+// constantes que indicam, posição, números, cor, comida e calculos.
+
+const incrementScore = () => {
+    score.innerText = +score.innerText + 10
+}
+
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food = {
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
+}
+
+let direction, loopId
+
+// Parte do desenho das comidas.
+
+const drawFood = () => {
+    const { x, y, color } = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 6
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size)
+    ctx.shadowBlur = 0
+}
+
+// Parte de desenho da snake.
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+        if (index == snake.length - 1) {
+            ctx.fillStyle = "white"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+// responsavel pelos movimentos da snake (Direção).
+
+const moveSnake = () => {
+    if (!direction) return
+
+    const head = snake[snake.length - 1]
+
+    if (direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+    }
+
+    if (direction == "left") {
+        snake.push({ x: head.x - size, y: head.y })
+    }
+
+    if (direction == "down") {
+        snake.push({ x: head.x, y: head.y + size })
+    }
+
+    if (direction == "up") {
+        snake.push({ x: head.x, y: head.y - size })
+    }
+
+    snake.shift()
+}
+
+// Desenho do grid ( marcando os degraus ceretinhos em 30 para a snake)
+
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }
+}
+
+// parte da comida para aumentar o score 
+
+const chackEat = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        incrementScore()
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+// responsavel pela parte de bater na parede (wallcollision) e em si mesma (selfcollision)
+
+const checkCollision = () => {
+    const head = snake[snake.length - 1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length - 2
+
+    const wallCollision =
+        head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+    })
+
+    if (wallCollision || selfCollision) {
+        gameOver()
+    }
+}
+
+// responsavel pelo fim do jogo ( + estilização com css )
+
+const gameOver = () => {
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerText
+    canvas.style.filter = "blur(2px"
+}
+
+// Essa parte significa a estrura, lógica de programação e loop.
+
+const gameLoop = () => {
+    clearInterval(loopId)
+
+    ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
+    moveSnake()
+    drawSnake()
+    chackEat()
+    checkCollision()
+
+    loopId = setTimeout(() => {
+        gameLoop()
+    }, 300)
+}
+
+gameLoop()
+
+// Responsavel pelos botoes de cliclar no teclado. 
+
+document.addEventListener("keydown", ({ key }) => {
+    if (key == "ArrowRight" && direction != "left") {
+        direction = "right"
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = "left"
+    }
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = "down"
+    }
+
+    if (key == "ArrowUp" && direction != "down") {
+        direction = "up"
+    }
+})
+
+// Parte Responsavel pelo score final e jogar novamente. 
+
+buttonPlay.addEventListener("click", () => {
+  score.innerText = "00"
+  menu.style.display = "none"
+  canvas.style.filter = "none"
+
+  snake = [{x: 270, y: 240}]
+})
