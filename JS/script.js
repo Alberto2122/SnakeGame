8ad66a618e1a35ca238c596147c1f6a55a27286a
Alberto Silva <135075028+Alberto2122@users.noreply.github.com>
2023-08-16 18:16:19 -0300

    atualização

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@ const finalScore = document.querySelector(".final-score > span")
 const menu = document.querySelector(".menu-screen")
 const buttonPlay = document.querySelector(".btn-play")
 
-const audio = new Audio("../jogo da cobra/audio/assets_audio.mp3")
+const audio = new Audio("../SnakeGame/assest/audio/assets_audio.mp3")
 
 const size = 30
 
